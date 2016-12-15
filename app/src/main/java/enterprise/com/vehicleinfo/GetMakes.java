11a876db98c87e036afc27fce9137331522274ae
mwilleke <mwilleke3@gmail.com>
2016-12-15 10:46:33 -0600

    getting values for makes from year asynchronously

---
+++ app/src/main/java/enterprise/com/vehicleinfo/GetMakes.java
@@ -0,0 +1,109 @@
+package enterprise.com.vehicleinfo;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Matthew on 12/15/2016.
+ */
+
+public class GetMakes extends AsyncTask<String, Void, List<String>> {
+
+    private Activity activity;
+    private ListFragment fragment;
+    private String year;
+    private List<String> makeList;
+
+    public GetMakes(ListFragment fragment, Activity activity) {
+        this.fragment = fragment;
+        this.activity = activity;
+        this.year = year;
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+    }
+
+    @Override
+    protected List<String> doInBackground(String... urls) {
+        try {
+            String strUrl = "";
+            for (String url : urls) {
+                strUrl = url;
+            }
+            URL url = new URL(strUrl);
+
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            String JSONString = streamToString(request.getInputStream());
+            JSONObject makeObject = new JSONObject(JSONString);
+            JSONArray array = makeObject.getJSONArray("makes");
+            makeList = new ArrayList<>();
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject childJSONObject = array.getJSONObject(i);
+                makeList.add(childJSONObject.getString("name"));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return makeList;
+    }
+
+    private String streamToString(InputStream inputStream) throws IOException {
+        if(inputStream != null) {
+            Writer writer = new StringWriter();
+
+            char[] buffer = new char[1024];
+
+            try {
+                Reader reader = new BufferedReader( new InputStreamReader(inputStream, "UTF-8"));
+
+                int n;
+                while((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            } finally {
+                inputStream.close();
+            }
+            return writer.toString();
+        } else {
+            return "";
+        }
+    }
+
+    @Override
+    protected void onPostExecute(List<String> result) {
+
+        fragment.setListAdapter(new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, makeList));
+    }
+}
--- app/src/main/java/enterprise/com/vehicleinfo/MakeFragment.java
+++ app/src/main/java/enterprise/com/vehicleinfo/MakeFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 /**
  * Created by Matthew on 12/14/2016.
@@ -16,6 +17,13 @@ public class MakeFragment extends android.support.v4.app.ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        Bundle bundle = getArguments();
+        String url = "https://api.edmunds.com/api/vehicle/v2/makes?year=" +
+                bundle.getString("year") + "&fmt=json&api_key=jskft3jqdm9wrhvj3fba2qwg";
+        new GetMakes(this, getActivity()).execute(url);
+       // GetYears yearsList = new GetYears();
+      //  ArrayAdapter<Integer> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, yearsList.getYearList(20));
+      //  setListAdapter(adapter);
         //GetYears yearsList = new GetYears();
         //ArrayAdapter<Integer> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, yearsList.getYearList(NUMBEROFYEARS));
         //setListAdapter(adapter);
